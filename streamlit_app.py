fb0d20811b0f075c529155a5c2621cb570439427
yewwah <yewwah10@gmail.com>
2023-06-20 17:25:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Expose the api to take in inputs 
+
+# This part to take in the upload file 
+
+# this part to take in the model 
+
+st.image('car dmg.png')
+st.file_uploader('upload_file')
 
